23cd3145b96be53e28825584458b2f57490e5d86
hx <tranhaobku2017@gmail.com>
2025-10-28 14:12:11 +0700

    ok on het roi

--- nvim/.config/nvim/lua/user/plugins/ui/nvimtree.lua
+++ nvim/.config/nvim/lua/user/plugins/ui/nvimtree.lua
@@ -3,94 +3,79 @@ return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+
   config = function()
-    -- Bắt buộc: tắt netrw để tránh xung đột
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-
     local nvim_tree = require("nvim-tree")
+    local api = require("nvim-tree.api")
 
+    -- 🧠 Setup
     nvim_tree.setup({
       sort_by = "case_sensitive",
-      sync_root_with_cwd = true,
-      respect_buf_cwd = true,
-
       view = {
         width = 32,
         side = "left",
-        adaptive_size = true,
         preserve_window_proportions = true,
         signcolumn = "yes",
       },
-
       renderer = {
         group_empty = true,
         highlight_git = true,
         highlight_opened_files = "name",
         indent_markers = { enable = true },
         icons = {
-          glyphs = {
-            folder = {
-              default = "",
-              open = "",
-              empty = "",
-              empty_open = "",
-            },
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = true,
           },
         },
       },
-
       filters = {
         dotfiles = false,
-        custom = { "^.git$", "node_modules" },
+        custom = { "^.git$", "node_modules", "dist" },
       },
-
       actions = {
         open_file = {
-          quit_on_open = true, -- tự đóng tree khi mở file
+          quit_on_open = false,          -- ✅ giữ tree khi mở file
           resize_window = true,
           window_picker = { enable = false },
         },
       },
-
       git = {
         enable = true,
         ignore = false,
         show_on_dirs = true,
-        timeout = 400,
       },
-
+      update_focused_file = {
+        enable = true,                  -- ✅ tự highlight file đang mở
+        update_cwd = true,
+      },
+      tab = {
+        sync = { open = true, close = true },
+      },
       diagnostics = {
         enable = true,
-        show_on_dirs = true,
-        icons = {
-          hint = "",
-          info = "",
-          warning = "",
-          error = "",
-        },
-      },
-
-      update_focused_file = {
-        enable = true,
-        update_root = true,
-        update_cwd = true,
+        show_on_dirs = false,
       },
     })
 
-    ------------------------------------------------------------------
-    -- 🔒 Auto close Nvim if NvimTree is last window
-    ------------------------------------------------------------------
-    vim.api.nvim_create_autocmd("BufEnter", {
-      nested = true,
-      callback = function()
-        if
-          #vim.api.nvim_list_wins() == 1
-          and vim.bo.filetype == "NvimTree"
-        then
-          vim.cmd("quit")
+    -- 🧩 Auto mở Tree khi vào Neovim
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function(data)
+        -- Nếu mở file, chỉ mở tree 1 lần
+        if vim.fn.isdirectory(data.file) == 1 then
+          vim.cmd.cd(data.file)
+          api.tree.open()
+        else
+          api.tree.open()
         end
       end,
     })
+
+    -- ⚙️ Keymaps chuyên nghiệp (Tree toggle & focus)
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", opts)  -- Bật/tắt tree
+    vim.keymap.set("n", "<leader>o", "<cmd>NvimTreeFocus<CR>", opts)   -- Nhảy focus sang tree
   end,
 }
