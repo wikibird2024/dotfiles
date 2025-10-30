479170e07901b2720c66c391112503a537ea3acb
hx <tranhaobku2017@gmail.com>
2025-10-30 23:24:06 +0700

    sua ts_comment

--- nvim/.config/nvim/lua/user/plugins/ts_comment.lua
+++ nvim/.config/nvim/lua/user/plugins/ts_comment.lua
@@ -1,12 +1,22 @@
 
+-- ~/.config/nvim/lua/user/plugins/ts_comment.lua
+-- Treesitter-aware comment plugin
+
 return {
   "folke/ts-comments.nvim",
   event = "VeryLazy",
-  enabled = vim.fn.has("nvim-0.10") == 1,
+  enabled = vim.fn.has("nvim-0.10") == 1, -- chỉ dùng nếu nvim >= 0.10
   opts = {
     lang = {
-      javascript = { "// %s", "/* %s */", jsx_element = "{/* %s */}", jsx_fragment = "{/* %s */}" },
+      javascript = {
+        "// %s",                 -- line comment
+        "/* %s */",             -- block comment
+        jsx_element = "{/* %s */}",
+        jsx_fragment = "{/* %s */}",
+      },
       typescript = "// %s",
+      lua = "-- %s",
+      python = "# %s",
     },
   },
 }
