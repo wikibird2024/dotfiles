224876278f85bc8d9910b8550ca20270fdd69b2c
hx <tranhaobku2017@gmail.com>
2025-12-25 09:08:20 +0700

    sua tot roi

--- nvim/.config/nvim/lua/user/plugins/tools/telescope.lua
+++ nvim/.config/nvim/lua/user/plugins/tools/telescope.lua
@@ -5,12 +5,6 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
 
-  keys = {
-    { "<leader>ff", "<cmd>Telescope find_files<cr>" },
-    { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
-    { "<leader>fb", "<cmd>Telescope buffers<cr>" },
-    { "<leader>fr", "<cmd>Telescope oldfiles<cr>" },
-  },
 
   config = function()
     local telescope = require("telescope")
