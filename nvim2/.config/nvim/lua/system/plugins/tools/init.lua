53cf08c3b533bbcabb926ac135db9dd29183e6b1
hx <tranhaobku2017@gmail.com>
2025-12-30 18:02:02 +0700

    ok them flash

--- nvim2/.config/nvim/lua/system/plugins/tools/init.lua
+++ nvim2/.config/nvim/lua/system/plugins/tools/init.lua
@@ -1,9 +1,9 @@
-
 -- ~/.config/nvim/lua/user/plugins/tools/init.lua
 return {
-  require("system.plugins.tools.fzf"),
-  require("system.plugins.tools.todo"),
-  require("system.plugins.tools.numb"),
-  require("system.plugins.tools.surround"),
-  require("system.plugins.tools.markdown"),
+	require("system.plugins.tools.fzf"),
+	require("system.plugins.tools.flash"),
+	require("system.plugins.tools.todo"),
+	require("system.plugins.tools.numb"),
+	require("system.plugins.tools.surround"),
+	require("system.plugins.tools.markdown"),
 }
