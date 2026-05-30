6a9beb10df83ed04010e36430655ff37ee4355f2
TranHao <tranhaobku2017@gmail.com>
2026-05-30 07:47:54 +0700

    FIX: fix review screen of telescope

--- nvim2/.config/nvim/lua/system/plugins/tools/telescope.lua
+++ nvim2/.config/nvim/lua/system/plugins/tools/telescope.lua
@@ -1,74 +1,85 @@
--- ~/.config/nvim/lua/system/plugins/tools/telescope.lua
-
 return {
-	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    cmd = "Telescope",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+        local telescope = require("telescope")
+        local actions = require("telescope.actions")
 
-	-- FIXED: Just like your old fzf config, this loads Telescope
-	-- automatically whenever a ":Telescope" command is executed!
-	cmd = "Telescope",
+        telescope.setup({
+            defaults = {
+                prompt_prefix = "   ",
+                selection_caret = "❯ ",
+                sorting_strategy = "ascending",
+                path_display = { "smart" },
 
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	},
+                -- Flex natively drops to vertical if width limits are hit
+                layout_strategy = "flex",
 
-	config = function()
-		local telescope = require("telescope")
-		local actions = require("telescope.actions")
+                layout_config = {
+                    -- Global static baselines
+                    width = 0.90,
+                    height = 0.85,
+                    preview_cutoff = 0, -- Prevents Telescope from dropping the preview pane entirely
 
-		telescope.setup({
-			defaults = {
-				prompt_prefix = "   ",
-				selection_caret = "❯ ",
-				sorting_strategy = "ascending",
-				path_display = { "truncate" },
-				layout_strategy = "horizontal",
+                    horizontal = {
+                        prompt_position = "top",
+                        preview_width = 0.55,
+                    },
+                    vertical = {
+                        mirror = true,
+                        prompt_position = "top",
+                        preview_height = 0.50, -- 50% of the height goes cleanly to preview
+                    },
+                    flex = {
+                        flip_columns = 120, -- Breakpoint width for stacking
+                    },
+                },
 
-				layout_config = {
-					horizontal = {
-						prompt_position = "top",
-						preview_width = 0.55,
-						results_width = 0.8,
-					},
-					width = 0.87,
-					height = 0.80,
-					preview_cutoff = 120,
-				},
+                mappings = {
+                    i = {
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-k>"] = actions.move_selection_previous,
+                        ["<Esc>"] = actions.close,
+                        ["<C-u>"] = false,
+                        -- Use C-f and C-b to read up/down inside file previews without switching windows
+                        ["<C-f>"] = actions.preview_scrolling_down,
+                        ["<C-b>"] = actions.preview_scrolling_up,
+                    },
+                    n = {
+                        ["q"] = actions.close,
+                        ["<C-f>"] = actions.preview_scrolling_down,
+                        ["<C-b>"] = actions.preview_scrolling_up,
+                    },
+                },
+            },
 
-				mappings = {
-					i = {
-						["<C-j>"] = actions.move_selection_next,
-						["<C-k>"] = actions.move_selection_previous,
-						["<Esc>"] = actions.close,
-						["<C-u>"] = false,
-					},
-					n = {
-						["q"] = actions.close,
-					},
-				},
-			},
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    no_ignore = false,
+                },
+                buffers = {
+                    sort_lastused = true,
+                    ignore_current_buffer = true,
+                    mappings = {
+                        i = { ["<C-d>"] = actions.delete_buffer },
+                        n = { ["d"] = actions.delete_buffer },
+                    },
+                },
+                live_grep = {
+                    previewer = true,
+                },
+                oldfiles = {
+                    prompt_title = "History",
+                },
+            },
+        })
 
-			pickers = {
-				find_files = {
-					previewer = true,
-					hidden = true,
-				},
-				buffers = {
-					sort_lastused = true,
-					previewer = true,
-					ignore_current_buffer = true,
-				},
-				live_grep = {
-					theme = "ivy",
-				},
-				oldfiles = {
-					prompt_title = "History",
-				},
-			},
-		})
-
-		-- Load the performance fzf extension compiled dependency
-		pcall(telescope.load_extension, "fzf")
-	end,
+        pcall(telescope.load_extension, "fzf")
+    end,
 }
