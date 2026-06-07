fe46cea2f82ac20b1a6159e6810669f98282aef4
hx <tranhaobku2017@gmail.com>
2026-06-07 23:22:07 +0700

    REMOVE: ouroboros

--- nvim2/.config/nvim/lua/system/plugins/tools/init.lua
+++ nvim2/.config/nvim/lua/system/plugins/tools/init.lua
@@ -3,7 +3,6 @@ return {
 	require("system.plugins.tools.fzf"),
 	require("system.plugins.tools.vim-tmux-navigator"),
 	require("system.plugins.tools.persistence"),
-	require("system.plugins.tools.ouroboros"),
 	require("system.plugins.tools.nvim-bqf"),
 	require("system.plugins.tools.nvim-dap"),
 	require("system.plugins.tools.crates"),
--- nvim2/.config/nvim/lua/system/plugins/tools/ouroboros.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"jakemason/ouroboros.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	ft = { "c", "cpp" }, -- Only load for C/C++ files
-	keys = {
-		{ "<leader>a", "<cmd>OuroborosTS<CR>", desc = "C/C++: Switch between Source/Header" },
-	},
-}
