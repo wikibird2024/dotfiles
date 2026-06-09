3a4e23a99ba61f417d0d688a5dd681fc3c2b7871
TranHao <tranhaobku2017@gmail.com>
2026-06-09 19:14:45 +0700

    ok

--- nvim2/.config/nvim/lua/system/plugins/colorscheme.lua
+++ nvim2/.config/nvim/lua/system/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ local themes = {
 		module = nil, -- It doesn't use require("gruvbox8").setup()
 		opts = {
 			-- We handle these via vim.g in the config function below
-			contrast = "hard", -- Options: soft, medium, hard
+			contrast = "medium", -- Options: soft, medium, hard
 			italic = 1,
 		},
 	},
