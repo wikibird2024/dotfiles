b405ff0fe7d7e42abe8ad51d1a8cd184421375aa
TranHao <tranhaobku2017@gmail.com>
2026-08-04 09:41:13 +0700

    fix copilot :

--- nvim2/.config/nvim/lua/system/plugins/tools/copilot.lua
+++ nvim2/.config/nvim/lua/system/plugins/tools/copilot.lua
@@ -1,8 +1,14 @@
+-- Path: nvim2/.config/nvim/lua/system/plugins/tools/copilot-cmp.lua
 return {
 	"zbirenbaum/copilot.lua",
 	cmd = "Copilot",
 	event = "InsertEnter",
 	opts = {
+		copilot_node_command = (function()
+			local nodes = vim.fn.glob("~/.nvm/versions/node/*/bin/node", false, true)
+			table.sort(nodes)
+			return nodes[#nodes] or "node"
+		end)(),
 		suggestion = {
 			enabled = true,
 			auto_trigger = true,
@@ -16,4 +22,5 @@ return {
 		},
 		panel = { enabled = false },
 	},
-} 
+}
+
